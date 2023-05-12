9dbf33653f0b2d8dba09056ea23034a9d1b43bf7
Tiburon_Blanco <antolini.martin@gmail.com>
2023-05-12 00:30:29 -0300

    FIX

--- src/main/java/com/martin/portfolio/config/WebConfig.java
+++ src/main/java/com/martin/portfolio/config/WebConfig.java
@@ -18,7 +18,7 @@ public class WebConfig {
 
         CorsConfiguration corsConfiguration = new CorsConfiguration();
         corsConfiguration.setAllowCredentials(true);
-        corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:4200", "https://nameless-leaf-2280.fly.dev"));
+        corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:4200", "https://portfolio-martin-antolini.web.app"));
         corsConfiguration.addAllowedMethod("*");
         corsConfiguration.addAllowedHeader("*");
         corsConfiguration.addExposedHeader("*");
