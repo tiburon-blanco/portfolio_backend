a75d1400dec2c824035e5133cf9e8e3eaf0b8fa5
Tiburon_Blanco <antolini.martin@gmail.com>
2023-05-14 18:34:19 -0300

    FIX

--- src/main/java/com/martin/portfolio/Application.java
+++ src/main/java/com/martin/portfolio/Application.java
@@ -2,7 +2,10 @@ package com.martin.portfolio;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @ComponentScan
@@ -12,4 +15,13 @@ public class Application {
 		SpringApplication.run(Application.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("https://portfolio-martin-antolini.web.app");
+			}
+		};
+	}
 }
